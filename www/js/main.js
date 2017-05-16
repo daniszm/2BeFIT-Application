956d3401744e44651da3d0e205259cea70194b74
michalkrysta <krysta.michal93@gmail.com>
2017-05-16 19:36:55 +0200

    Map is attached to the project

--- www/js/main.js
+++ www/js/main.js
@@ -60,6 +60,49 @@ function getTime() {
 // alert(objectString);
 // }
 
+/*
+ * Google Maps documentation: http://code.google.com/apis/maps/documentation/javascript/basics.html
+ * Geolocation documentation: http://dev.w3.org/geo/api/spec-source.html
+ */
+$(document).on("pageinit", "#map-page", function () {
+  var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
+  if (navigator.geolocation) {
+    function success(pos) {
+      // Location found, show map with these coordinates
+      drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+    }
+    function fail(error) {
+      drawMap(defaultLatLng);  // Failed to find location, show default map
+    }
+    // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+    navigator.geolocation.getCurrentPosition(success, fail, { maximumAge: 500000, enableHighAccuracy: true, timeout: 6000 });
+  } else {
+    drawMap(defaultLatLng);  // No geolocation support, show default map
+  }
+  function drawMap(latlng) {
+    var myOptions = {
+      zoom: 10,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      title: "ME",
+      //icon: 'pinkball.png'
+    });
+
+    var marker1 = new google.maps.Marker({
+      position: new google.maps.LatLng(50.07128, 19.93696),
+      map: map,
+      title: "Platinium Fitness!",
+      //icon:'hatnla.png'
+    });
+  }
+});
+
 $(document).ready(function () {
   
 
